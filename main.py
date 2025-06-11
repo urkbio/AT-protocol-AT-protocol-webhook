1ae4757a42f754db323a3a6d75482cd7528cb1f3
urkbio <uniquezyg@outlook.com>
2025-06-11 19:45:35 +0800

    first commit

--- main.py
+++ main.py
@@ -93,4 +93,4 @@ async def handle_webhook(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7361)
